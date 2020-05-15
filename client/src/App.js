f7113a6fd39c91e3ff311d1c38add69d61d69130
Emre Akurek <eakurek@mmds-MacBook-Pro.local>
2020-05-15 14:29:23 -0500

    completed client side

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Launches  from './components/Launches';
+import Launch from './components/Launch';
+import './App.css';
+import logo from './logoSpaceX.jpeg';
+
+const client = new ApolloClient({
+  uri: '/graphql'
+});
+
+
+const App = () => {
+  
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: 'block', margin: 'auto' }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+
+
+export default App;
