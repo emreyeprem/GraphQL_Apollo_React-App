d23e79d7a16c1f4ac0c9da38c068e607fa680844
Emre Akurek <eakurek@mmds-MacBook-Pro.local>
2020-05-15 14:22:54 -0500

    completed client side

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-//const cors = require('cors');
+const cors = require('cors');
 const schema = require('./schema');
 const path = require('path');
 
 const app = express();
 
 // Allow cross-origin
-// app.use(cors());
+app.use(cors());
 
 app.use(
   '/graphql',
